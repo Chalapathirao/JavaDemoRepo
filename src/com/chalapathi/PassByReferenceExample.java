309a166b1546256d8e2bb75acd1e966c6227b00a
Chalapathirao Davala <chalapathi.davala@gmail.com>
2024-09-02 21:27:48 +0530

    Pass by value vs reference

--- src/com/chalapathi/PassByReferenceExample.java
+++ src/com/chalapathi/PassByReferenceExample.java
@@ -3,8 +3,8 @@ package com.chalapathi;
 public class PassByReferenceExample {
     public static void main(String[] args) {
         Person person = new Person("Alice", 30);
-        System.out.println(person.getName()); // Output: Bob
-        System.out.println(person.getAge()); // Output: 40
+        System.out.println(person.getName()); // Output: Alice
+        System.out.println(person.getAge()); // Output: 30
 
         modifyPerson(person);
         System.out.println(person.getName()); // Output: Bob
