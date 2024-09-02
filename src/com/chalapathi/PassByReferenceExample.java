85654013c2c6d2f210805d5eff0e547c318bf950
Chalapathirao Davala <chalapathirao_davala@epam.com>
2024-09-02 21:24:02 +0530

    passby value and reference

---
+++ src/com/chalapathi/PassByReferenceExample.java
@@ -0,0 +1,17 @@
+package com.chalapathi;
+
+public class PassByReferenceExample {
+    public static void main(String[] args) {
+        Person person = new Person("Alice", 30);
+        modifyPerson(person);
+        System.out.println(person.getName()); // Output: Bob
+        System.out.println(person.getAge()); // Output: 40
+    }
+
+    public static void modifyPerson(Person p) {
+        p.setName("Bob");
+        p.setAge(40);
+    }
+}
+
+
---
+++ src/com/chalapathi/PassByValueExample.java
@@ -0,0 +1,14 @@
+package com.chalapathi;
+
+public class PassByValueExample {
+    public static void main(String[] args) {
+        int x = 10;
+        System.out.println(x); // Output: 10
+        modifyValue(x);
+        System.out.println(x); // Output: 10
+    }
+
+    public static void modifyValue(int y) {
+        y = 20;
+    }
+}
