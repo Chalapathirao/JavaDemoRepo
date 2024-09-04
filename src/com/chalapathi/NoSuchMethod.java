2afa50474951233cb21e349b0bd422af7f36f63e
Chalapathirao Davala <chalapathi.davala@gmail.com>
2024-09-04 10:20:16 +0530

    04-09-2024

---
+++ src/com/chalapathi/NoSuchMethod.java
@@ -0,0 +1,7 @@
+package com.chalapathi;
+
+public class NoSuchMethod {
+    public void main(String[] args) {
+        System.out.println("hi");
+    }
+}
---
+++ src/com/chalapathi/OrderOfExecution.java
@@ -0,0 +1,20 @@
+package com.chalapathi;
+
+public class OrderOfExecution {
+    static {
+        System.out.println("Static block executed.");
+    }
+
+    {
+        System.out.println("Instance initializer block executed.");
+    }
+
+    public OrderOfExecution() {
+        System.out.println("Constructor executed.");
+    }
+
+    public static void main(String[] args) {
+        OrderOfExecution obj1 = new OrderOfExecution();
+        OrderOfExecution obj2 = new OrderOfExecution();
+    }
+}
